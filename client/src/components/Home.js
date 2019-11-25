d694b33b3e6b74386069413edfae6d21782beb41
Suhana Ayisha <suhanaayisha@gmail.com>
2019-11-25 17:09:32 +0400

    typing effect for multiple lines

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,8 @@ class Home extends Component {
     render() {
         return(
             <div class="home typewriter" id="home">
-                <h1>Striving To Make A Difference</h1>
+                <h1>"What we have to learn to do,</h1>
+                <h1> we learn by doing." - Aristotle </h1>
             </div>
         )
     }
